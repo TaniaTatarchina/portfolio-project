94e2feb406334c78f63b737b84f94f1ad667f0af
TaniaTatarchina <zinya111@yahoo.com>
2019-05-03 17:08:51 +0300

    Blog functionality and polish

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from blog.models import Blog
+
+
+def allblogs(request):
+    blogs = Blog.objects.all()
+    return render(request, 'blog/allblogs.html', {'blogs': blogs})
+
+
+def detail(request, blog_id):
+    detail_blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': detail_blog})
